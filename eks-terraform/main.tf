645afa6e5a4118029d85fe8174dc3c3acb3cbd23
MeenakshiRuthna <meenakshiruthna@gmail.com>
2025-11-20 10:41:57 -0600

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -114,7 +114,7 @@ resource "aws_iam_instance_profile" "worker" {
 # ----------------------------
 data "aws_vpc" "main" {
   tags = {
-    Name = "Jumphost-vpc"
+    Name = "jumphosts-vpc"
   }
 }
 
@@ -138,7 +138,7 @@ data "aws_security_group" "selected" {
   vpc_id = data.aws_vpc.main.id
   filter {
     name   = "tag:Name"
-    values = ["Jumphost-sg"]
+    values = ["jumphosts-sg"]
   }
 }
 
