264b7ec63576a3c37098804c25c7058fff313667
MeenakshiRuthna <meenakshiruthna@gmail.com>
2025-11-20 12:53:49 -0600

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "jael0812"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "jael0812"
     Environment = "dev"
   }
 }
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "jael081290"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "jael081290"
     Environment = "dev"
   }
 }
