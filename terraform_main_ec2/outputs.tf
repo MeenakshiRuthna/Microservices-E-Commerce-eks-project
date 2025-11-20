d5d4eaf71d9b72bf390f1b91d366644be218ca68
MeenakshiRuthna <meenakshiruthna@gmail.com>
2025-11-20 12:50:14 -0600

    Update outputs.tf

--- terraform_main_ec2/outputs.tf
+++ terraform_main_ec2/outputs.tf
@@ -1,8 +1,8 @@
 output "region" {
-    description = "Jumphost Server region"
+    description = "Jumphost33 Server region"
     value = var.region
 }
-output "jumphost_public_ip" {
-  description = "Public IP address of the EC2 jumphost"
+output "jumphost33_public_ip" {
+  description = "Public IP address of the EC2 jumphost33"
   value       = aws_instance.ec2.public_ip
 }
